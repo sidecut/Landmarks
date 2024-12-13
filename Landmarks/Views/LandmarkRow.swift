ffc0f30fe4d9de31f16338f6e6986fc4801c00bd
Jim Raden <james.raden@gmail.com>
2024-12-13 00:54:25 -0500

    Customize row preview

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -12,11 +12,19 @@ struct LandmarkRow: View {
 
     var body: some View {
         HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
             Text(landmark.name)
+
+            Spacer()
         }
     }
 }
 
 #Preview {
-    LandmarkRow(landmark: landmarks[0])
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
