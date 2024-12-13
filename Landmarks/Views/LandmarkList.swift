c48e944614e36136a662756e01bd4ef15c7fc6a5
Jim Raden <james.raden@gmail.com>
2024-12-13 00:57:23 -0500

    Create the list of landmarks

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by James Raden on 12/13/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
