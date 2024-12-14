8da09ef1353387ea5a95fd9101c2f5b76212184d
Jim Raden <james.raden@gmail.com>
2024-12-14 02:23:46 -0500

    Add macOS target

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by James Raden on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
