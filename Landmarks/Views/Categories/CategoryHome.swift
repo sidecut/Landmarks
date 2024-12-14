79de442d2810384f1a944cba4769f22e14fb623d
Jim Raden <james.raden@gmail.com>
2024-12-13 21:25:01 -0500

    Complete the category view

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -14,11 +14,20 @@ struct CategoryHome: View {
 
         NavigationSplitView {
             List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                    Text(key)
+                    CategoryRow(
+                        categoryName: key, items: modelData.categories[key]!)
                 }
-                .navigationTitle("Featured")
+                .listRowInsets(EdgeInsets())
             }
+            .navigationTitle("Featured")
         } detail: {
             Text("Select a landmark")
         }
