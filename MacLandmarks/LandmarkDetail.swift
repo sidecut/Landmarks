ad0fb183f71155a0878135475972d5e83e231901
Jim Raden <james.raden@gmail.com>
2024-12-14 12:46:22 -0500

    Add new LandmarkDetail view to Mac views

---
+++ MacLandmarks/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  MacLandmarks
+//
+//  Created by James Raden on 12/14/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
