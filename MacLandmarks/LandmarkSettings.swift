821ba913f2a7e11dda4ff477604d0d4aad932aa6
Jim Raden <james.raden@gmail.com>
2024-12-15 23:05:58 -0500

    Create LandmarkSettings view

---
+++ MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by James Raden on 12/15/24.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LandmarkSettings()
+}
