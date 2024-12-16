9b2417361f9ac5f3c0a6265f2e417508f5c3b590
Jim Raden <james.raden@gmail.com>
2024-12-15 22:31:26 -0500

    Add keyboard shortcut

--- MacLandmarks/LandmarkCommands.swift
+++ MacLandmarks/LandmarkCommands.swift
@@ -17,6 +17,7 @@ struct LandmarkCommands: Commands {
             Button("\(selectedLandmark?.isFavorite == true ? "Remove" : "Mark") as Favorite") {
                 selectedLandmark?.isFavorite.toggle()
             }
+            .keyboardShortcut("f", modifiers: [.shift, .option])
             .disabled(selectedLandmark == nil)
         }
     }
